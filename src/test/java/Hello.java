2b6c838a20096b4995428127e097f08b5e0118f2
Mehmet-Afacan <mehmetafacan42@hotmail.com>
2020-10-13 18:53:03 +0100

    final commit after merge

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
   public static void main(String[] args) {
 
     System.out.println("Still really FROM GITHUB MADE SOME CHANGES awesome hello");//We are simulating a situation my coworker has change something
-    //somethng more added
+    //somethng more addedd
 
     //added one more line on my local
     //one more added daha
