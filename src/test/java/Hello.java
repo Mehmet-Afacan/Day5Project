8dc7e16c3bc0e0d3712b3a23213f62455ce2b08b
Mehmet-Afacan <72084505+Mehmet-Afacan@users.noreply.github.com>
2020-10-13 18:40:09 +0100

    added more changes by co-worker

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
   public static void main(String[] args) {
     
-    System.out.println("Still really FROM GITHUB MADE SOME CHANGES awesome hello");
+    System.out.println("Still really FROM GITHUB MADE SOME CHANGES awesome hello");//We are simulating a situation my coworker has change something
 
     //added one more line on my local
     //one more added daha
