e48c4de51bd16eb082fde18aad14fddb9cdb0ef7
Mehmet-Afacan <72084505+Mehmet-Afacan@users.noreply.github.com>
2020-10-13 18:33:01 +0100

    Update Hello.java

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
   public static void main(String[] args) {
     
-    System.out.println("Still really FROM GITHUB MADE SOME CHANGES awesome hello);
+    System.out.println("Still really FROM GITHUB MADE SOME CHANGES awesome hello");
 
     //added one more line on my local
  }
