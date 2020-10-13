dc568e14e45a7cb8e36bd38c13b1c6e963e1c26f
Mehmet-Afacan <mehmetafacan42@hotmail.com>
2020-10-13 18:41:54 +0100

    local hello

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
   public static void main(String[] args) {
     
-    System.out.println("Still really FROM GITHUB MADE SOME CHANGES awesome hello");
+    System.out.println("This is my own hello"); // this is my local hello, nad committing
 
     //added one more line on my local
     //one more added daha
